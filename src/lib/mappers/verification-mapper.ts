31721b36e95915fa1340bf5256c3da40fefe9268
Todd Snyder <tsnyder@gs1us.org>
2023-06-20 18:19:33 -0400

    Updated Core Output to include Resolve Credential Result

--- src/lib/mappers/verification-mapper.ts
+++ src/lib/mappers/verification-mapper.ts
@@ -90,8 +90,17 @@ export async function createVerificationResult(verifiedResults: any, gs1RulesRes
             }
 
             return credentialResult;
-        });
+    });
 
+    // Add Results for Resolve Credentials that are not in the presentation
+    gs1RulesResultContainer?.result?.forEach((gs1RulesResult: gs1RulesResult) => { 
+        const gs1RulesResultResolveCredential = result.credentialResults.find((result: gs1RulesResult) => result.credentialId === gs1RulesResult.credentialId);
+
+        if (!gs1RulesResultResolveCredential) { 
+            const {credential, ...resultUpdate} = gs1RulesResult;
+            result.credentialResults.push(resultUpdate);
+        }
+    });
     
     if (result.verifications) {
         // Add Verifier Type(s)
